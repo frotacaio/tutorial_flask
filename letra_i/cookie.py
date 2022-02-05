304d57c3d361df39394815759aff0b2d2e34e8f9
frotacaio <caiofrota@alu.ufc.br>
2022-02-05 10:26:52 -0300

    Initial commit

---
+++ letra_i/cookie.py
@@ -0,0 +1,21 @@
+from flask import Flask, make_response, render_template, request
+app = Flask(__name__)
+
+@app.route('/setcookie', methods = ['POST', 'GET'])
+def setcookie():
+    resp = make_response(render_template('readcookie.html'))
+    if request.method == 'POST':
+     user = request.form['nm']
+   
+     
+     resp.set_cookie('userID', user)
+    return resp
+
+
+@app.route('/getcookie')
+def getcookie():
+   name = request.cookies.get('userID')
+   return '<h1> Olá ' + name + '</h1>'
+
+if __name__ == '__main__':
+   app.run(debug = True)
