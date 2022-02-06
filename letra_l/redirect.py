7e425c4c44b148572ddd8d2dcb64e11beeb20135
frotacaio <caiofrota@alu.ufc.br>
2022-02-06 13:43:39 -0300

    Atividade
    Questões feitas recentes

--- letra_l/redirect.py
+++ letra_l/redirect.py
@@ -1,20 +1,26 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 # Initialize the Flask application
 app = Flask(__name__)
-
+ 
+ 
 @app.route('/')
 def index():
    return render_template('log_in.html')
-
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST' and request.form['username'] == 'admin' :
-    return redirect(url_for('success'))
-    return redirect(url_for('index'))
-
+    
+@app.route('/login',methods = ['POST', 'GET']) 
+def login(): 
+   if request.method == 'POST' and request.form['txtemail'] == 'caiofrota@gmail.com' and request.form['txtpass'] == 'secreto012' :
+      return redirect(url_for('success'))
+   else:
+      return redirect(url_for('errorlogin'))
+ 
 @app.route('/success')
 def success():
-   return 'logged in successfully'
-	
+   return '<h1>Logado com sucesso</h1>'
+  
+@app.route('/errorlogin')
+def errorlogin():
+   return '<h1>Credenciais erradas, tente novamente <a href = "/">login</a></h1>'
+    
 if __name__ == '__main__':
    app.run(debug = True)
---
+++ letra_m/extensions.py
@@ -0,0 +1,16 @@
+"""
+Extensões populares
+
+Flask Mail - Fornece uma interface SMTP para o aplicativo Flask
+
+Flask WTF - Adicione renderização e validação de WTForms
+
+Flask SQLAlchemy - Adicionando suporte SQLAlchemy para o aplicativo Flask
+
+Flask Sijax-Sijax - biblioteca de interface-Python/jQuery para tornar o AJAX fácil de usar em aplicações web
+
+
+"""
+import flaskext_compat
+flaskext_compat.activate()
+from flask.ext import foo
